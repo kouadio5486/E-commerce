ffc5d69833b891599b79c42a515c8bbac64bd09b
Kouadio5486 <paulinekouadio2020@gmail.com>
2025-10-25 17:14:44 +0000

    Ajout modèle

---
+++ ciacems/backend/apps/cart/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.conf import settings
+from products.models import Product
+
+class CartItem(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+# Create your models here.
---
+++ ciacems/backend/apps/products/models.py
@@ -0,0 +1,15 @@
+
+
+# Create your models here.
+from django.db import models
+from django.conf import settings
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField(default=0)
+
+class Favorite(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
